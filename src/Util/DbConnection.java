19d5d3ea0dcb28f157f42e51f039bd748b4d5173
Krishna Patel <krishnakanaujia07@gmail.com>
2026-06-16 16:00:25 +0530

    allows public key retrieval

--- src/Util/DbConnection.java
+++ src/Util/DbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/employee_management?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/employee_management?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "Krishna@2603";
 
